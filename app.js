8f68d778f0df46313b9d5706add262e6d50e7f84
Ruben Gonzales <argondesignsdev@gmail.com>
2022-09-25 19:54:13 -0700

    working

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 
+// Random number between 0 - 3 determines the computer's selection
 function getComputerChoice() {
- const rand = Math.floor(Math.random() * 3)+ 1
- 
+ const rand = Math.floor(Math.random() * 3);
+ let choice;
+ return rand === 0 ? choice = 'rock' : rand === 1 ? choice = 'paper' : rand === 2 ? choice = 'scissors' : null
 }
+
+// Get user input from a prompt
+function getPlayerChoice() {
+let playerChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
+return playerChoice;
+}
+
+// Plays one round of the game
+function playRound () {
+let computerChoice = getComputerChoice();
+let playerChoice = getPlayerChoice();
+
+if (playerChoice === 'rock' && computerChoice === 'paper' || playerChoice === 'scissors' && computerChoice === 'rock' || playerChoice === 'paper' && computerChoice === 'scissors') {
+  return 1
+} else if (playerChoice === computerChoice){
+  return 2
+} else {
+  return 3
+  }
+}
+
