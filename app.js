1ea097a3353849092250438b135b3b5f870d3131
Ruben Gonzales <argondesignsdev@gmail.com>
2022-09-25 21:48:30 -0700

    I think I finished it.

--- app.js
+++ app.js
@@ -12,17 +12,31 @@ let playerChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
 return playerChoice;
 }
 
-// Plays one round of the game
+// Plays one round of the game and determines the winner of the round
 function playRound () {
 let computerChoice = getComputerChoice();
 let playerChoice = getPlayerChoice();
 
-if (playerChoice === 'rock' && computerChoice === 'paper' || playerChoice === 'scissors' && computerChoice === 'rock' || playerChoice === 'paper' && computerChoice === 'scissors') {
-  return 1
+if (playerChoice === 'rock' && computerChoice === 'paper' || playerChoice === 'scissors' && computerChoice === 'rock' || playerChoice === 'paper' && computerChoice === 'scissors' || playerChoice === '') {
+  return [1, computerChoice, playerChoice];
 } else if (playerChoice === computerChoice){
-  return 2
+  return [2, computerChoice, playerChoice];
 } else {
-  return 3
+  return [3, computerChoice, playerChoice];
   }
 }
+// Plays 5 rounds of the game, keeps score, and announces the score as well as the selections.
+function playGame() { 
+let compWins = 0;
+let playerWins = 0;
+let tie = 0;
 
+for ( i = 0; i < 5; i++){ 
+  let results = playRound();
+  results[0] === 1 ? compWins++ : results[0] === 2 ? tie++ : results[0] === 3 ? playerWins++ : 'Error';
+  console.log(`Round ${i+1}: Computer: ${results[1]} Player: ${results[2]}`);
+}
+return `Computer Wins: ${compWins}, Player Wins: ${playerWins}, Ties: ${tie}`
+}
+
+console.log(playGame())
